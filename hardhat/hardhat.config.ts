cdeba13cc36a2cc22d18de5aa79c5632b5e5bbc1
Bhavya Gor <bhavya.gor9999@gmail.com>
2025-09-30 23:01:06 +0530

    hardhat inti

---
+++ hardhat/hardhat.config.ts
@@ -0,0 +1,18 @@
+import { config as dotenv } from "dotenv";
+dotenv();
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const { PRIVATE_KEY, BASE_SEPOLIA_RPC } = process.env;
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.24",
+  networks: {
+    baseSepolia: {
+      chainId: 84532,
+      url: BASE_SEPOLIA_RPC || "https://sepolia.base.org",
+      accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
+    },
+  },
+};
+export default config;
