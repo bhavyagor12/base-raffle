a97dfca8e0f5dadaeeb1eec2e1f591dee7de78ce
Bhavya Gor <bhavya.gor9999@gmail.com>
2025-10-04 01:07:19 +0530

    remove base sepolia

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,11 +1,10 @@
 import { http, createConfig } from "wagmi";
-import { base, baseSepolia } from "wagmi/chains";
+import { base } from "wagmi/chains";
 import { farcasterMiniApp as miniAppConnector } from "@farcaster/miniapp-wagmi-connector";
 
 export const config = createConfig({
-  chains: [baseSepolia, base],
+  chains: [base],
   transports: {
-    [baseSepolia.id]: http(),
     [base.id]: http(),
   },
   connectors: [miniAppConnector()],
